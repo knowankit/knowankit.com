296a8b97e08bd0cf5861b4d8cf4f3a84523256fd
knowankit <44323532+knowankit@users.noreply.github.com>
2021-07-23 06:44:03 +0530

    fix: code highlight

---
+++ components/layouts/blog-layout.tsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import Navbar from '@/components/navbar';
+import { Box } from '@chakra-ui/react';
+import { Global } from '@emotion/react';
+import { prismDarkTheme } from '@/styles/style';
+
+const BlogLayout = ({ children }) => {
+  return (
+    <Box>
+      <Global styles={prismDarkTheme} />
+      <Navbar />
+      <Box
+        mx="auto"
+        px="1rem"
+        my="2rem"
+        width={['18rem', '34rem', '48rem', '48rem']}
+        display="flex"
+        flexDirection="column">
+        {children}
+      </Box>
+    </Box>
+  );
+};
+
+export default BlogLayout;
