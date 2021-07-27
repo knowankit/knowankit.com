22bb330d23cbe01d22b5bb02b4b8ec52dcaed862
knowankit <44323532+knowankit@users.noreply.github.com>
2021-07-27 22:03:56 +0530

    feat: add icons

--- layouts/blog-layout.tsx
+++ layouts/blog-layout.tsx
@@ -4,6 +4,7 @@ import { Box } from '@chakra-ui/react';
 import { Global } from '@emotion/react';
 import { prismDarkTheme } from '@/styles/style';
 import FrontMatter from '@/components/front-matter';
+import Head from 'next/head';
 
 type Props = {
   children: JSX.Element;
@@ -12,21 +13,26 @@ type Props = {
 
 const BlogLayout: FC<Props> = ({ children, frontMatter }) => {
   return (
-    <Box className="mdx">
-      <Global styles={prismDarkTheme} />
-      <Navbar />
-      <Box
-        mx="auto"
-        px={['0.5rem', '1rem', '1rem', '1rem']}
-        py={['0.5rem', '1rem', '2rem', '2rem']}
-        my="2rem"
-        width={['full', '34rem', '48rem', '48rem']}
-        display="flex"
-        flexDirection="column">
-        <FrontMatter matter={frontMatter} />
-        {children}
+    <>
+      <Head>
+        <title>KnowAnkit - {frontMatter.title}</title>
+      </Head>
+      <Box className="mdx">
+        <Global styles={prismDarkTheme} />
+        <Navbar />
+        <Box
+          mx="auto"
+          px={['0.5rem', '1rem', '1rem', '1rem']}
+          py={['0.5rem', '1rem', '2rem', '2rem']}
+          my="2rem"
+          width={['full', '34rem', '48rem', '48rem']}
+          display="flex"
+          flexDirection="column">
+          <FrontMatter matter={frontMatter} />
+          {children}
+        </Box>
       </Box>
-    </Box>
+    </>
   );
 };
 
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,3 +1,14 @@
 import Projects from '@/components/projects';
+import Head from 'next/head';
 
-export default Projects;
+const ProjectsPage = () => {
+  return (
+    <>
+      <Head>
+        <title>KnowAnkit | Projects</title>
+      </Head>
+      <Projects />
+    </>
+  );
+};
+export default ProjectsPage;
