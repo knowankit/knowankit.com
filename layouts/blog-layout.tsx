50e53a422eb4176ed1f2327bb1377a6cfe5049ad
knowankit <44323532+knowankit@users.noreply.github.com>
2021-08-07 14:58:50 +0530

    feat: add og data

--- layouts/blog-layout.tsx
+++ layouts/blog-layout.tsx
@@ -5,6 +5,7 @@ import { Global } from '@emotion/react';
 import { prismDarkTheme } from '@/styles/style';
 import FrontMatter from '@/components/front-matter';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 
 type Props = {
   children: JSX.Element;
@@ -13,11 +14,33 @@ type Props = {
   isLightTheme: boolean;
 };
 
+const meta = {
+  title: 'KnowAnkit – Frontend Engineer, writer, creator.',
+  description: `JavaScript love, and course creator.`,
+  image: 'https://www.knowankit.com/myself.jpg',
+  type: 'website'
+};
+
 const BlogLayout: FC<Props> = ({ children, frontMatter }) => {
+  const router = useRouter();
+
   return (
     <>
       <Head>
-        <title>KnowAnkit - {frontMatter.title}</title>
+        <title>KnowAnkit - {meta.title}</title>
+        <meta name="robots" content="follow, index" />
+        <meta content={meta.description} name="description" />
+        <meta property="og:type" content={meta.type} />
+        <meta property="og:url" content={`https://knowankit.com${router.asPath}`} />
+        <link rel="canonical" href={`https://knowankit.com${router.asPath}`} />
+        <meta property="og:site_name" content="Know Ankit" />
+        <meta property="og:title" content={meta.title} />
+        <meta property="og:image" content={meta.image} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@knowankit" />
+        <meta name="twitter:title" content={meta.title} />
+        <meta name="twitter:description" content={meta.description} />
+        <meta name="twitter:image" content={meta.image} />
       </Head>
       <Box className="mdx">
         <Global styles={prismDarkTheme} />
