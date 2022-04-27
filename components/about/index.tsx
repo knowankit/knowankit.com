4be0bfd6d32207b25e625bdbadd9accab8965292
Ankit Kumar <ankit@bangthetable.com>
2022-04-27 06:36:05 +0530

    feat: add github calender

--- components/about/index.tsx
+++ components/about/index.tsx
@@ -2,12 +2,18 @@ import Navbar from '@/components/navbar';
 import Introduction from '@/components/about/introduction';
 import { Box } from '@chakra-ui/react';
 import Footer from '@/components/footer';
+import GitHubCalendar from 'react-github-calendar';
+import SocialIcons from '@/components/homepage/social-icons';
 
 const About = (): JSX.Element => {
   return (
     <Box minHeight="100vh" id="about">
       <Navbar />
       <Introduction />
+      <Box display='flex' justifyContent='center' alignItems='center'>
+        <GitHubCalendar username="knowankit" />
+      </Box>
+      <SocialIcons />
       <Footer />
     </Box>
   );
