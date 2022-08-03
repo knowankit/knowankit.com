39ca94ae381f8a0b0a7b3506654b3a38f540753d
Ankit Kumar <ankit@bangthetable.com>
2022-08-03 10:29:04 +0530

    fix: sizing of icons

--- components/homepage/social-icons.tsx
+++ components/homepage/social-icons.tsx
@@ -4,26 +4,18 @@ import { AiOutlineGithub, AiOutlineTwitter, AiFillLinkedin } from 'react-icons/a
 
 const SocialIcons = () => {
   return (
-    <Box display="flex" justifyContent="center" alignItems="center">
-      <Box
-        as="a"
-        fontSize={['1.4rem', '2.4rem', '3rem', '3rem']}
-        mr="20px"
-        href="https://github.com/knowankit">
+    <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      fontSize={['1.4rem', '2.4rem', '2.4rem', '2.4rem']}>
+      <Box as="a" mr="20px" color="#333" href="https://github.com/knowankit">
         <AiOutlineGithub />
       </Box>
-      <Box
-        as="a"
-        color="#1DA1F2"
-        fontSize={['1.4rem', '2.4rem', '3rem', '3rem']}
-        mr="20px"
-        href="https://twitter.com/knowankit">
+      <Box as="a" color="#1DA1F2" mr="20px" href="https://twitter.com/knowankit">
         <AiOutlineTwitter />
       </Box>
-      <Box
-        as="a"
-        fontSize={['1.4rem', '2.4rem', '3rem', '3rem']}
-        href="https://www.linkedin.com/in/knowankit/">
+      <Box as="a" color="#0A66C2" href="https://www.linkedin.com/in/knowankit/">
         <AiFillLinkedin />
       </Box>
     </Box>
